e8d7b5eba4c262a968c3d6de9b0f402ad7161210
asiryan <dmc5mod@yandex.ru>
2021-04-18 17:19:36 +0300

    correct solver

--- train/train_test.bat
+++ train/train_test.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist "snapshots\" md "snapshots"
-rem C:\caffe\caffe.exe train -solver solver.prototxt -log_dir=snapshots
+rem C:\caffe\caffe.exe train -solver solver_dpe_220_v4.prototxt -log_dir=snapshots
 rem C:\caffe\caffe.exe train -solver solver_dpe_220_v4.prototxt -weights snapshots\_iter_40000.caffemodel -log_dir=snapshots
 C:\caffe\caffe.exe test -model train_val_dpe_220_v4.prototxt -weights snapshots\_iter_40000.caffemodel -gpu 0 -iterations 100 -log_dir=snapshots
 pause
